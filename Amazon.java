f8c369c51b9e4c77770583f674e29221e47e0173
Gowtham R <gowthammodhaliyar59280@gmail.com>
2026-03-05 11:10:47 +0530

    example of amazon

---
+++ Amazon.java
@@ -0,0 +1,13 @@
+class Amazon {
+    public void createUserAccount(String name, String email,String password, String address,String city, String state,long phoneNumber)
+ {
+        System.out.println("Creating Amazon Account");
+        System.out.println("Name: " + name);
+        System.out.println("Email: " + email);
+        System.out.println("Password: " + password);
+        System.out.println("Address: " + address);
+        System.out.println("City: " + city);
+        System.out.println("State: " + state);
+        System.out.println("Phone: " + phoneNumber);
+    }
+}
